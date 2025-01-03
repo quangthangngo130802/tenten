92557f50b193776966c146d4e007638a7c393db2
thangngo.dev <thangngo130802@gmail.com>
2025-01-03 17:22:46 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -26,32 +26,32 @@ use Symfony\Component\HttpKernel\Profiler\Profile;
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
+| Here is where you can register web Routes for your application. These
+| Routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-route::middleware('guest')->group(function () {
-    route::get('', [AuthController::class, 'login'])->name('login');
-    route::post('', [AuthController::class, 'authenticate']);
-    route::get('dang-ky-tai-khoan', [AuthController::class, 'register'])->name('register');
-    route::post('dang-ky-tai-khoan', [AuthController::class, 'submitregister'])->name('submit.register');
-    route::get('reset-password', [AuthController::class, 'resetpass'])->name('resetpass');
-    route::post('reset-password', [AuthController::class, 'sendResetPassword'])->name('submit.resetpass');
+Route::middleware('guest')->group(function () {
+    Route::get('', [AuthController::class, 'login'])->name('login');
+    Route::post('', [AuthController::class, 'authenticate']);
+    Route::get('dang-ky-tai-khoan', [AuthController::class, 'register'])->name('register');
+    Route::post('dang-ky-tai-khoan', [AuthController::class, 'submitregister'])->name('submit.register');
+    Route::get('reset-password', [AuthController::class, 'resetpass'])->name('resetpass');
+    Route::post('reset-password', [AuthController::class, 'sendResetPassword'])->name('submit.resetpass');
     Route::get('/activate-account/{token}', [AuthController::class, 'activateAccount'])->name('activate.account');
 
 });
 
 Route::middleware('auth')->group(function () {
-    route::get('logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('logout', [AuthController::class, 'logout'])->name('logout');
 
-    route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+    Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
 
     Route::prefix('payment')->name('payment.')->group(function () {
-        route::get('', [PaymentController::class, 'recharge'])->name('recharge');
-        route::post('', [PaymentController::class, 'createPayment'])->name('recharge.add');
-        route::get('cancel', [PaymentController::class, 'cancelUrl'])->name('recharge.cancel');
-        route::get('return/{amount}', [PaymentController::class, 'returnUrl'])->name('recharge.return');
+        Route::get('', [PaymentController::class, 'recharge'])->name('recharge');
+        Route::post('', [PaymentController::class, 'createPayment'])->name('recharge.add');
+        Route::get('cancel', [PaymentController::class, 'cancelUrl'])->name('recharge.cancel');
+        Route::get('return/{amount}', [PaymentController::class, 'returnUrl'])->name('recharge.return');
 
     });
 
@@ -60,49 +60,49 @@ Route::middleware('auth')->group(function () {
 
 
         Route::prefix('user')->name('user.')->group(function () {
-            route::get('', [UserController::class, 'index'])->name('index');
-            route::get('create', [UserController::class, 'create'])->name('create');
-            route::post('', [UserController::class,'store'])->name('store');
-            route::get('{id}/edit', [UserController::class, 'edit'])->name('edit');
-            route::put('{id}/edit', [UserController::class, 'update'])->name('update');
-            route::post('{id}', [ClientController::class, 'delete'])->name('delete');
+            Route::get('', [UserController::class, 'index'])->name('index');
+            Route::get('create', [UserController::class, 'create'])->name('create');
+            Route::post('', [UserController::class,'store'])->name('store');
+            Route::get('{id}/edit', [UserController::class, 'edit'])->name('edit');
+            Route::put('{id}/edit', [UserController::class, 'update'])->name('update');
+            Route::post('{id}', [ClientController::class, 'delete'])->name('delete');
         });
 
         Route::prefix('client')->name('client.')->group(function () {
-            route::get('', [ClientController::class, 'index'])->name('index');
-            route::get('create', [ClientController::class, 'create'])->name('create');
-            route::post('', [ClientController::class,'store'])->name('store');
-            route::get('{id}/edit', [ClientController::class, 'edit'])->name('edit');
-            route::put('{id}/edit', [ClientController::class, 'update'])->name('update');
-            route::post('{id}', [ClientController::class, 'delete'])->name('delete');
+            Route::get('', [ClientController::class, 'index'])->name('index');
+            Route::get('create', [ClientController::class, 'create'])->name('create');
+            Route::post('', [ClientController::class,'store'])->name('store');
+            Route::get('{id}/edit', [ClientController::class, 'edit'])->name('edit');
+            Route::put('{id}/edit', [ClientController::class, 'update'])->name('update');
+            Route::post('{id}', [ClientController::class, 'delete'])->name('delete');
         });
 
         Route::prefix('order')->name('order.')->group(function () {
-            route::get('{status?}', [OrderController::class, 'index'])->name('index');
-            route::get('{id}/show', [OrderController::class, 'show'])->name('show');
-            route::post('delete-{id}', [OrderController::class, 'delete'])->name('delete');
-            route::post('active-{id}', [OrderController::class, 'active'])->name('active');
+            Route::get('{status?}', [OrderController::class, 'index'])->name('index');
+            Route::get('{id}/show', [OrderController::class, 'show'])->name('show');
+            Route::post('delete-{id}', [OrderController::class, 'delete'])->name('delete');
+            Route::post('active-{id}', [OrderController::class, 'active'])->name('active');
 
         });
 
         Route::prefix('hosting')->name('hosting.')->group(function () {
-            route::get('', [HostingController::class, 'index'])->name('index');
-            route::get('create', [HostingController::class, 'create'])->name('create');
-            route::post('', [HostingController::class,'store'])->name('store');
-            route::get('{id}/edit', [HostingController::class, 'edit'])->name('edit');
-            route::put('{id}/edit', [HostingController::class, 'update'])->name('update');
-            route::post('delete-{id}', [HostingController::class, 'delete'])->name('delete');
+            Route::get('', [HostingController::class, 'index'])->name('index');
+            Route::get('create', [HostingController::class, 'create'])->name('create');
+            Route::post('', [HostingController::class,'store'])->name('store');
+            Route::get('{id}/edit', [HostingController::class, 'edit'])->name('edit');
+            Route::put('{id}/edit', [HostingController::class, 'update'])->name('update');
+            Route::post('delete-{id}', [HostingController::class, 'delete'])->name('delete');
         });
         Route::prefix('cloud')->name('cloud.')->group(function () {
-            route::get('add', [CloudController::class, 'create'])->name('create');
-            route::get('{type_id?}', [CloudController::class, 'index'])->name('index');
-            route::post('', [CloudController::class,'store'])->name('store');
-            route::get('{id}/edit', [CloudController::class, 'edit'])->name('edit');
-            route::put('{id}/edit', [CloudController::class, 'update'])->name('update');
-            route::post('{id}', [CloudController::class, 'delete'])->name('delete');
+            Route::get('add', [CloudController::class, 'create'])->name('create');
+            Route::get('{type_id?}', [CloudController::class, 'index'])->name('index');
+            Route::post('', [CloudController::class,'store'])->name('store');
+            Route::get('{id}/edit', [CloudController::class, 'edit'])->name('edit');
+            Route::put('{id}/edit', [CloudController::class, 'update'])->name('update');
+            Route::post('{id}', [CloudController::class, 'delete'])->name('delete');
         });
 
-        route::post('delete-{id}', [TransactionHistoryController::class, 'delete'])->name('history.delete');
+        Route::post('delete-{id}', [TransactionHistoryController::class, 'delete'])->name('history.delete');
 
         Route::prefix('domain')->name('domain.')->group(function () {
             Route::get('', [DomainController::class, 'index'])->name('index');
@@ -123,29 +123,29 @@ Route::middleware('auth')->group(function () {
     Route::prefix('customer')->name('customer.')->group(function () {
 
         Route::prefix('hosting')->name('hosting.')->group(function () {
-            route::get('', [CustomerHostingController::class, 'index'])->name('index');
+            Route::get('', [CustomerHostingController::class, 'index'])->name('index');
         });
 
         Route::prefix('cloud')->name('cloud.')->group(function () {
-            route::get('{type_id?}', [CustomerCloudController::class, 'index'])->name('index');
-            route::get('vi/cloud-{id}', [CustomerCloudController::class, 'vicloud'])->name('vicloud');
-            route::post('add-cart', [CustomerCloudController::class, 'addtocart'])->name('addtocart');
+            Route::get('{type_id?}', [CustomerCloudController::class, 'index'])->name('index');
+            Route::get('vi/cloud-{id}', [CustomerCloudController::class, 'vicloud'])->name('vicloud');
+            Route::post('add-cart', [CustomerCloudController::class, 'addtocart'])->name('addtocart');
         });
 
         Route::prefix('order')->name('order.')->group(function () {
-            route::get('show/{id?}', [CustomerOrderController::class, 'addorder'])->name('payment');
-            route::get('{status?}', [CustomerOrderController::class, 'index'])->name('index');
-            route::get('{id}/show', [CustomerOrderController::class, 'show'])->name('show');
-            route::post('thanh-toan/don-hang', [CustomerOrderController::class, 'thanhtoan'])->name('thanhtoan');
+            Route::get('show/{id?}', [CustomerOrderController::class, 'addorder'])->name('payment');
+            Route::get('{status?}', [CustomerOrderController::class, 'index'])->name('index');
+            Route::get('{id}/show', [CustomerOrderController::class, 'show'])->name('show');
+            Route::post('thanh-toan/don-hang', [CustomerOrderController::class, 'thanhtoan'])->name('thanhtoan');
             Route::get('vi/prcode/{id}/{xsd}', [CustomerOrderController::class, 'createPayment'])->name('create.payment');
-            route::get('return/{id}/{xsd}', [CustomerOrderController::class, 'returnUrl'])->name('return');
+            Route::get('return/{id}/{xsd}', [CustomerOrderController::class, 'returnUrl'])->name('return');
 
         });
         Route::prefix('cart')->name('cart.')->group(function () {
-            // route::get('', [CartController::class, 'listcart'])->name('listcart');
-            route::get('', [CartController::class, 'listcart'])->name('listcart');
-            route::post('renews-ervice-{id}', [RenewServiceController::class, 'addrenews'])->name('addrenews');
-            route::get('renews-ervice', [RenewServiceController::class, 'listrenews'])->name('listrenews');
+            // Route::get('', [CartController::class, 'listcart'])->name('listcart');
+            Route::get('', [CartController::class, 'listcart'])->name('listcart');
+            Route::post('renews-ervice-{id}', [RenewServiceController::class, 'addrenews'])->name('addrenews');
+            Route::get('renews-ervice', [RenewServiceController::class, 'listrenews'])->name('listrenews');
         });
 
         Route::prefix('service')->name('service.')->group(function () {
@@ -162,8 +162,8 @@ Route::middleware('auth')->group(function () {
 
     });
 
-    route::get('profile', [ProfileController::class, 'profile'])->name('profile');
-    route::post('profile', [ProfileController::class, 'updateprofile'])->name('profile.update');
+    Route::get('profile', [ProfileController::class, 'profile'])->name('profile');
+    Route::post('profile', [ProfileController::class, 'updateprofile'])->name('profile.update');
 
 });
 
@@ -178,8 +178,8 @@ Route::post('/delete-item', [CartController::class, 'deleteItem'])->name('delete
 Route::post('/checkout', [CartController::class, 'checkout'])->name('checkout.item');
 
 Route::prefix('history')->name('history.')->group(function () {
-    route::get('{status?}', [TransactionHistoryController::class, 'index'])->name('index');
-    route::get('{id}/show', [TransactionHistoryController::class, 'show'])->name('show');
+    Route::get('{status?}', [TransactionHistoryController::class, 'index'])->name('index');
+    Route::get('{id}/show', [TransactionHistoryController::class, 'show'])->name('show');
 });
 
 Route::post('/clear-pdf-session', [CustomerOrderController::class, 'clearPdfSession'])->name('clear.pdf.session');
